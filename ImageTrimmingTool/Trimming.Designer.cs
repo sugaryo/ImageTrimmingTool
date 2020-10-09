5040a5b60e5d69c16710f22b186f31d501a004a1
sugaryo <ryo.sugawara.1224@gmail.com>
2020-10-09 11:04:20 +0900

    スコープを変更。

--- ImageTrimmingTool/Trimming.Designer.cs
+++ ImageTrimmingTool/Trimming.Designer.cs
@@ -23,15 +23,12 @@ namespace ImageTrimmingTool {
             }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("90")]
         public int Quality {
             get {
                 return ((int)(this["Quality"]));
-            }
-            set {
-                this["Quality"] = value;
             }
         }
     }
